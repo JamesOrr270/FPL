39afef3004404bd3dda2d64a7df9549b263deb15
JamesOrr270 <jamesnorr@hotmail.co.uk>
2025-10-31 10:53:19 +0000

    Remove data from github

--- data_download.py
+++ data_download.py
@@ -1,13 +1,12 @@
 import requests
 import pandas as pd
 
-# Example: Get general player data
 url = "https://fantasy.premierleague.com/api/bootstrap-static/"
 data = requests.get(url).json()
 
-players = pd.DataFrame(data['elements'])   # All player info
-teams = pd.DataFrame(data['teams'])        # Teams info
-events = pd.DataFrame(data['events'])      # Gameweek info
+players = pd.DataFrame(data['elements'])  
+teams = pd.DataFrame(data['teams'])        
+events = pd.DataFrame(data['events'])      
 
 players.to_csv("players.csv", index=False)
 teams.to_csv("teams.csv", index=False)
